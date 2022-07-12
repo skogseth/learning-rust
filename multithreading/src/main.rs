c86a8131b4a97d2bcb3da4c4ffe2dace4aabefe7
skogseth <herman.skogseth@me.com>
2022-07-12 13:43:20 +0200

    16.2

--- multithreading/src/main.rs
+++ multithreading/src/main.rs
@@ -1,7 +1,14 @@
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 
 fn main() {
+    //basics();
+
+    channels();
+}
+
+fn basics() {
     let mut vec: Vec<_> = (1..10).collect();
     let handle = thread::spawn(move || {
         for _ in 1..10 {
@@ -16,4 +23,41 @@ fn main() {
     }
 
     handle.join().unwrap();
+}
+
+fn channels() {
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
