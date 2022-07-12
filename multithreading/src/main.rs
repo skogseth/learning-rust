b2227b4d7df60cfdf7f41365abb0925141a7858e
skogseth <herman.skogseth@me.com>
2022-07-12 13:25:34 +0200

    16.1

--- multithreading/src/main.rs
+++ multithreading/src/main.rs
@@ -1,3 +1,19 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let mut vec: Vec<_> = (1..10).collect();
+    let handle = thread::spawn(move || {
+        for _ in 1..10 {
+            println!("hi number {} from the spawned thread!", vec.pop().unwrap());
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    handle.join().unwrap();
 }
