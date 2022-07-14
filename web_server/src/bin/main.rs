fe78a8078face78b30f06fce197941d20478ac5c
skogseth <herman.skogseth@me.com>
2022-07-14 15:04:43 +0200

    20.3 + some more

--- web_server/src/bin/main.rs
+++ web_server/src/bin/main.rs
@@ -3,6 +3,7 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
@@ -10,19 +11,42 @@ use web_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(4);
+    listener.set_nonblocking(true).unwrap();
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
+    let pool = ThreadPool::new(7);
+    let run = Arc::new(Mutex::new(true));
+
+    let run_ptr = Arc::clone(&run);
+    let handle = thread::spawn(|| {
+        handle_input(run_ptr);
+    });
+
+    let run_ptr = Arc::clone(&run);
+    while *run_ptr.lock().unwrap() == true {
+        let stream = match listener.accept() {
+            Ok((socket, _addr)) => socket,
+            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => continue,
+            Err(e) => panic!("encountered IO error: {}", e),
+        };
+
         pool.execute(|| {
             handle_connection(stream);
         });
     }
+
+    handle.join().expect("Unable to shut down input thread");
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
+    if let Err(e) = stream.read(&mut buffer) {
+        if e.kind() == std::io::ErrorKind::WouldBlock {
+            return;
+        } else {
+            panic!("encountered IO error: {}", e);
+        }
+    }
 
     let get = b"GET / HTTP/1.1\r\n";
     let sleep = b"GET /sleep HTTP/1.1\r\n";
@@ -36,7 +60,7 @@ fn handle_connection(mut stream: TcpStream) {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
+    let contents = fs::read_to_string(filename).expect("Failed to read .html file");
 
     let response = format!(
         "{}\r\nContent-Length: {}\r\n\r\n{}",
@@ -45,6 +69,32 @@ fn handle_connection(mut stream: TcpStream) {
         contents
     );
 
-    stream.write(response.as_bytes()).unwrap();
+    stream
+        .write(response.as_bytes())
+        .expect("Failed to write to TcpStream");
     stream.flush().unwrap();
+}
+
+fn handle_input(run_ptr: Arc<Mutex<bool>>) {
+    loop {
+        let mut input = String::new();
+
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input line");
+
+        let input: String = match input.trim().parse() {
+            Ok(s) => s,
+            Err(_) => {
+                println!("Bad input");
+                continue;
+            }
+        };
+
+        if input == ":q" {
+            let mut b = run_ptr.lock().unwrap();
+            *b = false;
+            break;
+        }
+    }
 }
--- web_server/src/lib.rs
+++ web_server/src/lib.rs
@@ -2,12 +2,17 @@ use std::sync::mpsc;
 use std::sync::{Arc, Mutex};
 use std::thread;
 
-pub struct ThreadPool {
-    workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+enum Message {
+    NewJob(Job),
+    Terminate,
 }
 
-type Job = Box<dyn FnOnce() + Send + 'static>;
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Message>,
+}
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -37,26 +42,49 @@ impl ThreadPool {
     where
         F: FnOnce() + Send + 'static,
     {
-        let job = Box::new(f);
+        let message = Message::NewJob(Box::new(f));
 
-        self.sender.send(job).unwrap();
+        self.sender.send(message).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutting down all workers.");
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
     }
 }
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-    pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
+    pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
+        let thread_loop = move || loop {
+            let message = receiver.lock().unwrap().recv().unwrap();
 
-            println!("Worker {} got a job; executing.", id);
+            match message {
+                Message::NewJob(job) => job(),
+                Message::Terminate => break,
+            }
+        };
 
-            job();
-        });
+        let thread = Some(thread::spawn(thread_loop));
 
         Worker { id, thread }
     }
