124817df6f1dd526ecd8470f913f95d4979126d4
skogseth <herman.skogseth@me.com>
2022-07-14 08:42:06 +0200

    Raw pointers

--- advanced_features/src/main.rs
+++ advanced_features/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
+
+    let mut a: i32 = 5;
+
+    let a_ptr: *mut i32 = &mut a;
+    let b_ptr: *mut i32 = a_ptr;
+
+    unsafe {
+        *a_ptr = 10;
+        println!("value: {}", *b_ptr);
+    }
 }
