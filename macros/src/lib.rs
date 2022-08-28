1771b57ed80812fd24222cb4787b7baf8ccb280c
skogseth <herman.skogseth@me.com>
2022-08-28 17:05:57 +0200

    Added (optional) type functionality to add macro

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,5 +1,14 @@
 #[macro_export]
 macro_rules! add {
+    ( $( $x:expr ),* ; $y:ty ) => {
+        {
+            let mut sum = 0;
+            $(
+                sum += $x as $y;
+            )*
+            sum
+        }
+    };
     ( $( $x:expr ),* ) => {
         {
             let mut sum = 0;
--- macros/src/main.rs
+++ macros/src/main.rs
@@ -1,14 +1,17 @@
-use macros::add;
+use macros::*;
 
 fn main() {
     println!("Hello, world!");
 
-    let a = add!(1, 2, 3, 4);
+    let a = add!(1, 2, 3);
     let b = add!(2);
     let c = add!();
-    
+
     println!("a = {a}");
     println!("a = {b}");
     println!("a = {c}");
+
+    let d = add!(8, 16; u8);
+    println!("d = {d}");
 }
 
