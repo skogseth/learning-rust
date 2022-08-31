30604699c07f1fa66b667b39e0fd93dc05fa7b09
skogseth <herman.skogseth@me.com>
2022-08-31 10:58:48 +0200

    Smarter add macro

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,21 +1,19 @@
 #[macro_export]
 macro_rules! add {
-    ( $( $x:expr ),* ; $y:ty ) => {
+    ( $( $x:expr ),+ ; $y:ty ) => {
         {
-            let mut sum = 0;
+            0
             $(
-                sum += $x as $y;
+                + $x as $y
             )*
-            sum
         }
     };
-    ( $( $x:expr ),* ) => {
+    ( $( $x:expr ),+ ) => {
         {
-            let mut sum = 0;
+            0
             $(
-                sum += $x;
+                + $x
             )*
-            sum
         }
     };
 }
--- macros/src/main.rs
+++ macros/src/main.rs
@@ -5,11 +5,10 @@ fn main() {
 
     let a = add!(1, 2, 3);
     let b = add!(2);
-    let c = add!();
+    // let c = add!(); // compilation error
 
     println!("a = {a}");
     println!("a = {b}");
-    println!("a = {c}");
 
     let d = add!(8, 16; u8);
     println!("d = {d}");
