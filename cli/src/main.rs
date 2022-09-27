0056fcd7c278a458106885f7e0335697e3c00a77
skogseth <herman.skogseth@me.com>
2022-09-27 20:28:14 +0200

    Played around a bit with anyhow, log and env_logger

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,40 +1,68 @@
 use clap::Parser;
-use tokio::fs::File;
-use tokio::io::AsyncWriteExt;
+use env_logger::Builder;
+use log::LevelFilter;
+
+use anyhow::Error as AnyError;
 
 #[derive(Parser, Debug)]
 #[clap(version)]
 struct Args {
     #[clap(value_parser)]
-    number_of_values: usize,
+    max_value: usize,
 
     #[clap(short, long, value_parser, default_value = "data.txt")]
     filename: String,
+
+    /// Set verbosity
+    #[clap(short, long, action = clap::ArgAction::Count)]
+    verbosity: u8,
 }
 
-#[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
+
+fn main() -> Result<(), AnyError> {
     let args = Args::parse();
 
-    let vals = (0..args.number_of_values)
-        .filter_map(|x| {
-            let squared = x * x;
-            if squared % 2 == 0 {
-                Some(squared)
-            } else {
-                None
-            }
-        })
-        .collect::<Vec<_>>();
+    let log_level = match args.verbosity {
+        0 => LevelFilter::Error,
+        1 => LevelFilter::Info,
+        2 => LevelFilter::Debug,
+        _ => LevelFilter::Trace,
+    };
+
+    Builder::new().filter_level(log_level).init();
+
+    log::info!("hello world");
+    log::warn!("this is dangerous!!");
+    
+    let vals = some_squared(args.max_value);
 
     let mut str = String::new();
     for val in vals {
+        log::debug!("val: {}", val);
         str += &val.to_string();
         str.push('\t');
     }
 
-    let mut file = File::create(args.filename).await?;
-    file.write_all(str.as_bytes()).await?;
+    if args.filename != "data.txt" {
+        log::debug!("filename: {}", args.filename);
+        log::error!("FUUUUUUCK");
+        anyhow::bail!("abandon ship!")
+    }
 
     Ok(())
+}
+
+fn some_squared(max_value: usize) -> Vec<i32> {
+    (0..max_value)
+        .filter_map(|x| {
+            let x = x as i32;
+            let squared = x * x;
+            if squared % 2 == 0 {
+                Some(squared)
+            } else {
+                log::info!("value {} was filtered out", squared);
+                None
+            }
+        })
+        .collect::<Vec<_>>()
 }
